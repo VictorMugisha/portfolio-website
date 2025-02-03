a708eb55cb0738209ab8aaf3b784f34269e1023b
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2025-02-03 15:09:22 +0200

    Implemented App Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
+import { useEffect } from "react";
+import Aos from "aos";
+
+import "./App.css";
+import Header from "./components/Header/Header";
+import Hero from "./components/UI/Hero";
+import Services from "./components/UI/Services";
+import Portfolio from "./components/UI/Portfolio";
+import Contact from "./components/UI/Contact";
+import Footer from "./components/Footer/Footer";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  useEffect(() => {
+    Aos.init();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <main>
+        <Hero />
+        <Services />
+        <Portfolio />
+        <Contact />
+      </main>
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
