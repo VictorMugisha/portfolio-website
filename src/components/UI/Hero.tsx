b3f128330873216553adf8d972862e482f484163
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2025-02-03 16:10:31 +0200

    Hero and Service Sections - Some Styling Still Yet To Be Done

--- src/components/UI/Hero.tsx
+++ src/components/UI/Hero.tsx
@@ -1,5 +1,163 @@
+import heroImg from "../../assets/images/profile.png";
+import CountUp from "react-countup";
+
 export default function Hero() {
   return (
-    <div>Hero</div>
-  )
+    <section className="pt-0" id="about">
+      <div className="container pt-14">
+        <div className="md:flex items-center justify-between sm:flex-col md:flex-row">
+          {/* Hero Left Content */}
+          <div className="w-full md:basis-1/2">
+            <h5
+              data-aos="fade-right"
+              data-aos-duration="1500"
+              className="text-headingColor font-[600] text-[16px]"
+            >
+              Hello welcome
+            </h5>
+
+            <h1
+              data-aos="fade-up"
+              data-aos-duration="1500"
+              className="text-headingColor font-[800] text-[1.8rem] sm:text-[40px] leading-[35px] sm:leading-[46px] mt-5"
+            >
+              Hello, I'm Victor Mugisha <br />
+              Software Developer
+            </h1>
+
+            <div
+              data-aos="fade-up"
+              data-aos-duration="1800"
+              data-aos-delay="200"
+              className="flex items-center gap-6 mt-7"
+            >
+              <a href="#contact">
+                <button className="bg-primaryColor text-white font-[500] flex items-center gap-2 hover:bg-smallTextColor ease-in duration-200 py-2 px-4 rounded-[8px]">
+                  <i className="ri-mail-line"></i> Hire me
+                </button>
+              </a>
+              <a
+                href="#portfolio"
+                className="text-smallTextColor font-[600] text-[16px] border-b border-solid border-smallTextColor"
+              >
+                See portfolio
+              </a>
+            </div>
+            <p
+              data-aos="fade-left"
+              data-aos-duration="1500"
+              className="flex gap-2 text-headingColor mt-10 font-[500] text-[15px] leading-7 sm:pl-14 sm:pr-10"
+            >
+              <span>
+                <i className="ri-apps-2-line"></i>
+              </span>
+              Welcome to my digital space! Victor Mugisha, a passionate student
+              developer from Kigali, exploring the realms of web technologies.
+            </p>
+
+            <div className="flex items-center gap-9 mt-10">
+              <span className="text-smallTextColor font-[600] text-[15px]">
+                Follow me:
+              </span>
+              <span>
+                <a
+                  href="http://discordapp.com/users/1188580923038519388"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-smallTextColor text-[18px] font-[600]"
+                >
+                  <i className="ri-discord-line"></i>
+                </a>
+              </span>
+              <span>
+                <a
+                  href="https://github.com/VictorMugisha"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-smallTextColor text-[18px] font-[600]"
+                >
+                  <i className="ri-github-fill"></i>
+                </a>
+              </span>
+              <span>
+                <a
+                  href="https://www.linkedin.com/in/victor-mugisha-shyaka-47b10b233/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-smallTextColor text-[18px] font-[600]"
+                >
+                  <i className="ri-linkedin-line"></i>
+                </a>
+              </span>
+              <span>
+                <a
+                  href="https://www.instagram.com/victor_mugisha001/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-smallTextColor text-[18px] font-[600]"
+                >
+                  <i className="ri-instagram-line"></i>
+                </a>
+              </span>
+            </div>
+          </div>
+
+          {/* Hero Img*/}
+          <div className="basis-1/2 mt-10 sm:mt-0 flex justify-center relative">
+            {/* Animated Background Accent */}
+            <div className="absolute w-[250px] h-[250px] bg-gradient-to-r from-blue-400 to-purple-500 opacity-30 rounded-full blur-3xl -z-10 animate-pulse"></div>
+
+            {/* Hero Image Container */}
+            <div
+              className="relative w-[80%] max-w-[350px] h-auto overflow-hidden rounded-full border-4 border-transparent bg-gradient-to-r from-purple-500 via-blue-400 to-indigo-500 p-[4px] shadow-2xl 
+    before:absolute before:inset-0 before:bg-white before:rounded-full before:z-[-1] before:opacity-20 before:blur-xl 
+    hover:scale-105 transition-transform duration-500 ease-in-out animate-float"
+            >
+              <img
+                src={heroImg}
+                className="w-full h-full object-cover rounded-full"
+                alt="Victor Mugisha"
+              />
+            </div>
+          </div>
+
+          {/* ============= Hero Right Content ============= */}
+          <div className="md:basis-1/5 flex justify-between text-center mt-10 flex-wrap gap-3 md:mt-0 md:flex-col md:justify-end md:text-end">
+            <div className="mb-10">
+              <h2 className="text-headingColor font-[700] text-[32px]">
+                <CountUp start={0} end={4} duration={2} suffix="+" />
+              </h2>
+              <h4 className="text-headingColor font-[600] text-[18px]">
+                Years of Experience
+              </h4>
+            </div>
+            <div className="mb-10">
+              <h2 className="text-headingColor font-[700] text-[32px]">
+                <CountUp start={0} end={100} duration={2} suffix="%" />
+              </h2>
+              <h4 className="text-headingColor font-[600] text-[18px]">
+                Success Rate
+              </h4>
+            </div>
+            <div className="mb-10">
+              <h2 className="text-headingColor font-[700] text-[32px]">
+                <CountUp start={0} end={150} duration={2} suffix="+" />
+              </h2>
+              <h4 className="text-headingColor font-[600] text-[18px]">
+                Happy Clients
+              </h4>
+            </div>
+            <div className="mb-10">
+              <h2 className="text-headingColor font-[700] text-[32px]">
+                <CountUp start={0} end={185} duration={2} suffix="+" />
+              </h2>
+              <h4 className="text-headingColor font-[600] text-[18px]">
+                Projects Completed
+              </h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/components/UI/Services.tsx
+++ src/components/UI/Services.tsx
@@ -1,5 +1,151 @@
+import frontEndImg from "../../assets/images/front-end.png";
+import backendImg from "../../assets/images/backend.png";
+import uiImg from "../../assets/images/design.png";
+import appsImg from "../../assets/images/apps.png";
+
 export default function Services() {
   return (
-    <div>Services</div>
-  )
+    <section id="services">
+      <div className="container lg:pt-10 flex flex-col items-center justify-center gap-10">
+        <div className="text-center flex flex-col items-center justify-center gap-10">
+          <h2 className="text-headingColor font-extrabold text-3xl md:text-4xl mb-6">
+            What I do
+          </h2>
+          <p className="lg:max-w-xl mx-auto text-headingColor font-medium text-base leading-7">
+            Crafting immersive web experiences in Kigali, Rwanda. Leveraging
+            HTML, CSS, JavaScript, ReactJS, and Tailwind CSS to architect
+            dynamic, user-centric solutions. Elevating web standards with
+            innovation and dedication.
+          </p>
+        </div>
+
+        <div className="flex flex-col justify-center sm:py-12">
+          <div className="w-full py-6 px-4 sm:max-w-xl sm:mx-auto sm:px-0">
+            <div className="relative text-gray-700 antialiased text-sm font-semibold">
+              {/* Vertical line in the center */}
+              <div className="hidden absolute w-1 sm:block bg-indigo-300 h-full left-1/2 -translate-x-1/2"></div>
+
+              {/* Left Card */}
+              <div className="mt-10 sm:mt-0 sm:mb-16">
+                <div className="flex sm:flex-row">
+                  <div className="flex justify-start w-full mx-auto sm:mr-10">
+                    <div className="w-full sm:w-1/2">
+                      <div
+                        data-aos="fade-right"
+                        data-aos-duration="1200"
+                        className="bg-white p-10 rounded-xl shadow-lg group hover:bg-primaryColor cursor-pointer transition duration-300"
+                      >
+                        <h3 className="text-primaryColor font-bold text-xl mb-3 group-hover:text-white">
+                          Responsive Web Design
+                        </h3>
+                        <p className="text-sm text-smallTextColor group-hover:text-white leading-6">
+                          Tailored, visually stunning websites ensuring seamless
+                          user experience across all devices and screen sizes.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="rounded-full bg-primaryColor border-white border-4 w-12 h-12 absolute left-1/2 -translate-x-1/2 -translate-y-5 sm:translate-y-0 flex items-center justify-center">
+                    <figure>
+                      <img src={frontEndImg} alt="Frontend" />
+                    </figure>
+                  </div>
+                </div>
+              </div>
+
+              {/* Right Card */}
+              <div className="mt-10 sm:mt-0 sm:mb-16">
+                <div className="flex sm:flex-row">
+                  <div className="flex justify-end w-full mx-auto sm:ml-10">
+                    <div className="w-full sm:w-1/2">
+                      <div
+                        data-aos="fade-left"
+                        data-aos-delay="50"
+                        data-aos-duration="1300"
+                        className="bg-white p-5 rounded-xl shadow-lg group hover:bg-primaryColor cursor-pointer transition duration-300"
+                      >
+                        <h3 className="text-primaryColor font-bold text-xl mb-3 group-hover:text-white">
+                          Interactive UI Development
+                        </h3>
+                        <p className="text-sm text-smallTextColor group-hover:text-white leading-6">
+                          Engaging interfaces crafted with ReactJS, enhancing
+                          user interaction and functionality.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="rounded-full bg-primaryColor border-white border-4 w-12 h-12 absolute left-1/2 -translate-x-1/2 -translate-y-5 sm:translate-y-0 flex items-center justify-center">
+                    <figure>
+                      <img src={backendImg} alt="Backend" />
+                    </figure>
+                  </div>
+                </div>
+              </div>
+
+              {/* Left Card */}
+              <div className="mt-10 sm:mt-0 sm:mb-16">
+                <div className="flex sm:flex-row">
+                  <div className="flex justify-start w-full mx-auto sm:mr-10">
+                    <div className="w-full sm:w-1/2">
+                      <div
+                        data-aos="fade-right"
+                        data-aos-duration="1200"
+                        className="bg-white p-5 rounded-xl shadow-lg group hover:bg-primaryColor cursor-pointer transition duration-300"
+                      >
+                        <h3 className="text-primaryColor font-bold text-xl mb-3 group-hover:text-white">
+                          Performance Optimization
+                        </h3>
+                        <p className="text-sm text-smallTextColor group-hover:text-white leading-6">
+                          Utilizing modern web techniques to boost website speed
+                          and efficiency for better performance.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="rounded-full bg-primaryColor border-white border-4 w-12 h-12 absolute left-1/2 -translate-x-1/2 -translate-y-5 sm:translate-y-0 flex items-center justify-center">
+                    <figure>
+                      <img src={uiImg} alt="UI Design" />
+                    </figure>
+                  </div>
+                </div>
+              </div>
+
+              {/* Right Card */}
+              <div className="mt-10 sm:mt-0 sm:mb-16">
+                <div className="flex sm:flex-row">
+                  <div className="flex justify-end w-full mx-auto sm:ml-10">
+                    <div className="w-full sm:w-1/2">
+                      <div
+                        data-aos="fade-left"
+                        data-aos-delay="100"
+                        data-aos-duration="1400"
+                        className="bg-white p-5 rounded-xl shadow-lg group hover:bg-primaryColor cursor-pointer transition duration-300"
+                      >
+                        <h3 className="text-primaryColor font-bold text-xl mb-3 group-hover:text-white">
+                          Custom Component Development
+                        </h3>
+                        <p className="text-sm text-smallTextColor group-hover:text-white leading-6">
+                          Tailwind CSS and ReactJS expertise in creating
+                          modular, scalable frontend elements.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="rounded-full bg-primaryColor border-white border-4 w-12 h-12 absolute left-1/2 -translate-x-1/2 -translate-y-5 sm:translate-y-0 flex items-center justify-center">
+                    <figure>
+                      <img src={appsImg} alt="Apps" />
+                    </figure>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
