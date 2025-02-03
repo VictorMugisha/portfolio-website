8928481c220c224a0ae7a5a5c68ce0f9c7b2c34d
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2025-02-03 15:47:34 +0200

    Header is almost done

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,133 @@
+import { useRef, useEffect } from "react";
+
 export default function Header() {
+  const headerRef = useRef<HTMLElement | null>(null);
+  const menuRef = useRef<HTMLDivElement | null>(null);
+
+  const stickyHeaderFunc = () => {
+    if (
+      document.body.scrollTop > 80 ||
+      document.documentElement.scrollTop > 80
+    ) {
+      headerRef.current?.classList.add("sticky_header");
+    } else {
+      headerRef.current?.classList.remove("sticky_header");
+    }
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      stickyHeaderFunc();
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    e.preventDefault();
+    const targetAttr = (e.target as HTMLAnchorElement).getAttribute("href");
+
+    if (targetAttr) {
+      const targetElement = document.querySelector(
+        targetAttr
+      ) as HTMLElement | null;
+
+      if (targetElement) {
+        const location = targetElement.offsetTop;
+        window.scrollTo({
+          top: location - 80,
+          left: 0,
+          behavior: "smooth",
+        });
+      }
+    }
+  };
+
+  const toggleMenu = () => {
+    menuRef.current?.classList.toggle("show__menu");
+  };
+
   return (
-    <div>Header</div>
-  )
+    <header
+      ref={headerRef}
+      className="w-full h-[80px] leading-[80px] flex items-center"
+    >
+      <div className="container">
+        <div className="flex flex-center justify-between">
+          {/* Logo */}
+          <div className="flex items-center gap-[10px]">
+            <span className="w-[35px] h-[35px] bg-primaryColor text-white text-[18px] font-[500] rounded-full flex items-center justify-center">
+              V
+            </span>
+            <div className="leading-[20px]">
+              <h2 className="text-xl text-smallTextColor font-[700]">Victor</h2>
+              <p className="text-smallTextColor text-[14px] font-[500]">
+                Developer
+              </p>
+            </div>
+          </div>
+
+          {/* Menu */}
+          <div className="menu" ref={menuRef}>
+            <ul className="flex items-center gap-10">
+              <li>
+                <a
+                  onClick={handleClick}
+                  className="text-smallTextColor font-[600]"
+                  href="#about"
+                >
+                  About
+                </a>
+              </li>
+              <li>
+                <a
+                  onClick={handleClick}
+                  className="text-smallTextColor font-[600]"
+                  href="#services"
+                >
+                  Services
+                </a>
+              </li>
+              <li>
+                <a
+                  onClick={handleClick}
+                  className="text-smallTextColor font-[600]"
+                  href="#portfolio"
+                >
+                  Portfolio
+                </a>
+              </li>
+              <li>
+                <a
+                  onClick={handleClick}
+                  className="text-smallTextColor font-[600]"
+                  href="#contact"
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          {/* Menu Right */}
+          <div className="flex items-center gap-4">
+            <button className="flex items-center gap-2 text-smallTextColor fot-[600] border border-solid border-smallTextColor py-2 px-4 rounded-[8px] max-h-[40px] hover:bg-smallTextColor hover:text-white hover:font-[500] ease-in duration-200">
+              <i className="ri-send-plane-line"></i> Let's Chat
+            </button>
+            <span
+              onClick={toggleMenu}
+              className="text-2xl text-smallTextColor cursor-pointer md:hidden"
+            >
+              <i className="ri-menu-line"></i>
+            </span>
+          </div>
+
+        </div>
+      </div>
+    </header>
+  );
 }
